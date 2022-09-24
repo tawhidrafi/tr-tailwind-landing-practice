6abb16f23ff4d35dbe80c4afdcc70ab204d1105a
Tawhid Rafi <tawhidrafi196@gmail.com>
2022-09-25 01:59:16 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+//MOBILE MENU
+const btn = document.getElementById('menu-btn'),
+    nav = document.getElementById('menu');
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('open');
+    nav.classList.toggle('hidden');
+})
